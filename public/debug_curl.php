b8f22b35b7bb65c571fa2c6c969ddfcac2a2b61a
bootuv <elberd06@gmail.com>
2025-12-13 21:28:56 +0300

    delete debug

--- public/debug_curl.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "PHP Version: " . phpversion() . "<br>";
-echo "CURL Extension: " . (extension_loaded('curl') ? 'LOADED' : 'MISSING') . "<br>";
-echo "CURLOPT_SSL_VERIFYPEER: " . (defined('CURLOPT_SSL_VERIFYPEER') ? constant('CURLOPT_SSL_VERIFYPEER') : 'UNDEFINED') . "<br>";
-echo "php.ini loaded: " . php_ini_loaded_file() . "<br>";
