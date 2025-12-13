74fbf7e27e1097b91abd8ac2e6e2949ebebf371b
bootuv <elberd06@gmail.com>
2025-12-13 23:39:44 +0300

    refactor: switch `RoomStatusUpdated` event to a public channel and remove channel authentication logging.

--- routes/channels.php
+++ routes/channels.php
@@ -3,6 +3,5 @@
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('rooms', function ($user) {
-    \Illuminate\Support\Facades\Log::info('Channel auth requested', ['user' => $user->id ?? 'null']);
     return true;
 });
