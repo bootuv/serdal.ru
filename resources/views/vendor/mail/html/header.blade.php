215ea49ab2a4c15cd57eae52c1a7081dd652cf38
bootuv <elberd06@gmail.com>
2025-12-19 21:36:16 +0300

    Rename Logo.png to logo.png (lowercase) and update email template

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="https://serdal.ru/images/Logo.png" class="logo" alt="Serdal Logo"
+            <img src="https://serdal.ru/images/logo.png" class="logo" alt="Serdal Logo"
                 style="height: 32px; width: auto;">
         </a>
     </td>
