94787ee3c265c529545ee50e791dc6f995a79c2c
bootuv <elberd06@gmail.com>
2025-12-19 18:34:03 +0300

    Serdal logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,8 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{ asset('images/Logo.svg') }}" class="logo" alt="Serdal Logo" style="height: 48px; width: auto;">
+            <img src="https://serdal.ru/images/Logo.svg" class="logo" alt="Serdal Logo"
+                style="height: 48px; width: auto;">
         </a>
     </td>
 </tr>
