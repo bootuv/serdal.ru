15c9fc525a774f2ae0ba9ef7cf3c1947d521d160
bootuv <elberd06@gmail.com>
2025-12-19 18:37:55 +0300

    feat: Conditionally embed mail header logo or fall back to external URL.

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,8 +2,13 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="https://serdal.ru/images/Logo.svg" class="logo" alt="Serdal Logo"
-                style="height: 48px; width: auto;">
+            @if(isset($message) && method_exists($message, 'embed') && file_exists(public_path('images/Logo.svg')))
+                <img src="{{ $message->embed(public_path('images/Logo.svg')) }}" class="logo" alt="Serdal Logo"
+                    style="height: 48px; width: auto;">
+            @else
+                <img src="https://serdal.ru/images/Logo.svg" class="logo" alt="Serdal Logo"
+                    style="height: 48px; width: auto;">
+            @endif
         </a>
     </td>
 </tr>
