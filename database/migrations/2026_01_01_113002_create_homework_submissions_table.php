4829f145cf2689790bf56db9d782d4aec7082ed0
bootuv <elberd06@gmail.com>
2026-01-01 17:33:47 +0300

    refactor: explicitly constrain `homework_id` foreign key to `homeworks` table.

--- database/migrations/2026_01_01_113002_create_homework_submissions_table.php
+++ database/migrations/2026_01_01_113002_create_homework_submissions_table.php
@@ -17,7 +17,7 @@ return new class extends Migration {
 
         Schema::create('homework_submissions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('homework_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('homework_id')->constrained('homeworks')->cascadeOnDelete();
             $table->foreignId('student_id')->constrained('users')->cascadeOnDelete();
             $table->text('content')->nullable();
             $table->json('attachments')->nullable();
