9ddbebcc238a369c51945935348d46ea3498eab5
bootuv <elberd06@gmail.com>
2026-01-01 21:31:11 +0300

    refactor: Simplify x-init scroll to bottom logic in chat components.

--- resources/views/livewire/support-chat-component.blade.php
+++ resources/views/livewire/support-chat-component.blade.php
@@ -105,7 +105,7 @@
             <div class="flex-1 overflow-y-auto p-4 space-y-4 relative"
                 x-ref="chatContainer" id="support-messages-container"
                 x-data="{ imageModal: false, imageUrl: '' }" 
-                x-init="$nextTick(() => { $nextTick(() => { $el.scrollTop = $el.scrollHeight; }); });"
+                x-init="$el.scrollTop = $el.scrollHeight"
                 @message-sent.window="$nextTick(() => $el.scrollTop = $el.scrollHeight)"
                 @message-received.window="$nextTick(() => $el.scrollTop = $el.scrollHeight)">
                 
